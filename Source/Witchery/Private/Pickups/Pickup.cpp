d172eb8856690f90096b3b4311ed25da0a3c134a
Liemander <liemander16@gmail.com>
2023-06-19 19:42:32 +0300

    11 - 1

---
+++ Source/Witchery/Private/Pickups/Pickup.cpp
@@ -0,0 +1,62 @@
+// Witchery. Copyright Liemander. All Rights Reserved.
+
+#include "Pickups/Pickup.h"
+#include "Components/SphereComponent.h"
+#include "Sound/SoundCue.h"
+#include "WTRTypes.h"
+
+APickup::APickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+
+	AreaSphere = CreateDefaultSubobject<USphereComponent>("AreaSphere");
+    AreaSphere->SetupAttachment(GetRootComponent());
+    AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    AreaSphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+    AreaSphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+    AreaSphere->SetSphereRadius(150.f);
+
+	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
+    PickupMesh->SetCollisionResponseToAllChannels(ECR_Ignore);
+    PickupMesh->SetCustomDepthStencilValue(CUSTOM_DEPTH_PURPLE);
+    PickupMesh->MarkRenderStateDirty();
+    PickupMesh->SetupAttachment(AreaSphere);
+    EnableCustomDepth(true);
+
+	bReplicates = true;
+}
+
+void APickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (HasAuthority())
+    {
+        AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereBeginOverlap);
+    }
+}
+
+void APickup::Destroyed() 
+{
+    Super::Destroyed();
+}
+
+void APickup::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+    int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void APickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APickup::EnableCustomDepth(bool bEnable)
+{
+    if (PickupMesh)
+    {
+        PickupMesh->SetRenderCustomDepth(bEnable);
+    }
+}
---
+++ Source/Witchery/Public/Pickups/Pickup.h
@@ -0,0 +1,41 @@
+// Witchery. Copyright Liemander. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+class USphereComponent;
+class USoundCue;
+class UStaticMeshComponent;
+
+UCLASS()
+class WITCHERY_API APickup : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    APickup();
+    virtual void Tick(float DeltaTime) override;
+
+protected:
+    virtual void BeginPlay() override;
+    virtual void Destroyed() override;
+
+    UFUNCTION()
+    virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+        int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+private:
+    UPROPERTY(VisibleAnywhere, Category = "WTR | Components")
+    USphereComponent* AreaSphere;
+
+    UPROPERTY(VisibleAnywhere, Category = "WTR | Components")
+    UStaticMeshComponent* PickupMesh;
+
+    UPROPERTY(EditDefaultsOnly, Category = "WTR | Sound")
+    USoundCue* PickupSound;
+
+    void EnableCustomDepth(bool bEnable);
+};
